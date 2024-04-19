c3f82cdc5d6b6170f4a3ba7b634f9547c2ef9051
LeeSangYong <lsy3709@gmail.com>
2024-04-19 12:37:04 +0900

    커밋 테스트4

---
+++ Java_Work/src/ex_240419/Hello4.java
@@ -0,0 +1,26 @@
+package ex_240419;
+
+public class Hello4 {
+
+	public static void main(String[] args) {
+		// 규칙1, 메인 함수(메서드)에서 시작하고, 끝난다.
+		// 규칙2, 파일명, 클래스명이 동일 해야한다.
+		// 글자 폰트 크기 단축키 : ctrl + shift + "+" 또는 "-"
+		// 코딩창 만 보기 : ctrl + m
+		// 실행 디버깅 없이 : ctrl + f11
+		System.out.println("Hello Java World");
+		// 클래스 파일 만들고, 오늘 먹을 점심 메뉴 출력해보기.
+
+		// 자동 완성.
+		// 1 sysout 단축키 : 참고 자료
+		// https://sycareer.tistory.com/entry/%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%EC%9E%90%EB%B0%94-sysout-%EC%9E%90%EB%8F%99%EC%99%84%EC%84%B1-%EC%95%88%EB%90%A0-%EB%95%8C-Systemoutprintln-%EC%9E%90%EB%8F%99-%EC%99%84%EC%84%B1
+
+		// 2 자동 완성 설정 참고 자료
+		// https://velog.io/@zz1996zz/Eclipse%EC%9D%B4%ED%81%B4%EB%A6%BD%EC%8A%A4-%EC%9E%90%EB%8F%99%EC%99%84%EC%84%B1-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
+
+		// 3 자동 줄 맞춤
+		// 단축키 : ctrl + shift + f
+
+	}
+
+}
