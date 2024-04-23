7fe82a85598093e59d924a2627ab524f54c00e8d
LeeSangYong <lsy3709@gmail.com>
2024-04-23 10:24:36 +0900

    240423 2교시 저장 1, 기초 예약어 소개 중

---
+++ Java_Work/src/ex_240423/homework/LeeSangYong.java
@@ -0,0 +1,10 @@
+package ex_240423.homework;
+
+public class LeeSangYong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("이상용 숙제 제출 240422");
+	}
+
+}
