104f1f6ec4d2c7c138bfbef2f9966d1dfedc6f7c
LeeSangYong <lsy3709@gmail.com>
2024-04-24 09:25:49 +0900

    240424 1교시 스캐너 복습및 확인

--- Java_Work/src/ex_240423_3/ScannerTest.java
+++ Java_Work/src/ex_240423_3/ScannerTest.java
@@ -13,17 +13,23 @@ public class ScannerTest {
 		// 사용법 , Scanner 클래스 객체를 생성, A a = new A(System.in);
 		Scanner scanner = new Scanner(System.in);
 		
+
+		// scanner: 객체, 수납도구, 읽은 내용을 숫자, 또는 문자열로 변환 해줍니다.
+		// scanner 입력 도구로, 문자열을 읽어서, 임시 메모리에 담아 두는 작업.
 		System.out.println("이름이 뭐예요?");
-		System.out.println("어디 사나요?");
-		
-		
-		// scanner: 객체, 수납도구, 읽은 내용을 숫자, 또는 문자열로 변환 해줍니다. 
 		String nameString = scanner.next();
 		
+		System.out.println("어디 사나요?");
 		String addressString = scanner.next();
 		
+		
+		System.out.println("좋아하는 숫자 : 1 ~ 10 중 하나 선택?");
+		// 숫자를 읽어서, 잠시 메모리에 임시 담아두기
+		int likeNumber = scanner.nextInt();
+				
 		System.out.println("이름 : " + nameString);
 		System.out.println("사는 곳 : " + addressString);
+		System.out.println("1 ~ 10 중 좋아하는 숫자는 : " + likeNumber);
 		
 		// 객체를 사용 후 , 반납. 
 		scanner.close();
