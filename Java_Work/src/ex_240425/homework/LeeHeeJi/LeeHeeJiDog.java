da1aeab3134d358eb06c8c1bd25b6eb2832f1e7a
heeji-lee <dpgbanjdal@gmail.com>
2024-04-25 14:28:25 +0900

    5교시 숙제 완료

---
+++ Java_Work/src/ex_240425/homework/LeeHeeJi/LeeHeeJiDog.java
@@ -0,0 +1,21 @@
+package ex_240425;
+
+public class LeeHeeJiDog extends LeeHeeJiAnimal {
+	
+	private String place;
+	private String type;
+	
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
