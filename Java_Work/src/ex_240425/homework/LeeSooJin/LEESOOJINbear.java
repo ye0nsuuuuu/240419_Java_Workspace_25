87d29314d3663a1562c3ac74b1708e8bb96d27ef
LeeSooJin <gowl0918@gmail.com>
2024-04-25 14:18:41 +0900

    Merge branch 'homework' of https://github.com/soooooojin/240419_Java_Workspace_fork.git into homework 이수진 ANIMAL 완료

---
+++ Java_Work/src/ex_240425/homework/LeeSooJin/LEESOOJINbear.java
@@ -0,0 +1,20 @@
+package ex_240425.homework.LeeSooJin;
+
+public class LEESOOJINbear extends LEESOOJINAnimal{
+	
+	private String kindOf;
+	private String color;
+	public String getKindOf() {
+		return kindOf;
+	}
+	public void setKindOf(String kindOf) {
+		this.kindOf = kindOf;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	} 
+	
+}
