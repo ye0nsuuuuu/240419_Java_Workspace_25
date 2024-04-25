6c21d254ddccf7a15cc676c8e970432c3b1d56dc
KODOKYUN <hwasungnj3@gmail.com>
2024-04-25 14:17:41 +0900

    240425 고도균 숙제 완료

---
+++ Java_Work/src/ex_240425/homework/Kodokyun/KodokyunCat.java
@@ -0,0 +1,25 @@
+package ex_240425.homework.Kodokyun;
+
+public class KodokyunCat extends KodokyunAnimal{
+	
+		private String place;
+		private String todayDinner;
+		
+		
+		public String getPlace() {
+			return place;
+		}
+		public void setPlace(String place) {
+			this.place = place;
+		}
+		public String getTodayDinner() {
+			return todayDinner;
+		}
+		public void setTodayDinner(String todayDinner) {
+			this.todayDinner = todayDinner;
+		}
+		
+		
+	}
+
+
