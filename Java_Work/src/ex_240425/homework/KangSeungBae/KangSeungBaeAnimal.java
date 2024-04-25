a3ead6da49978b2263d72b0676ed608da6b937fd
KANGSEUNBAE <KANGSEUNGBAE00@gmail.com>
2024-04-25 14:40:08 +0900

    강승배홈워크

---
+++ Java_Work/src/ex_240425/homework/KangSeungBae/KangSeungBaeAnimal.java
@@ -0,0 +1,45 @@
+package ex_240425.homework.KangSeungBae;
+
+import java.util.Arrays;
+
+public class KangSeungBaeAnimal {
+
+	private String name;
+	private int age;
+	private String[] favoriteFood;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String[] getFavoriteFood() {
+		return favoriteFood;
+	}
+	public void setFavoriteFood(String[] favoriteFood) {
+		this.favoriteFood = favoriteFood;
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [name=" + name + ", age=" + age + ", favoriteFood=" + Arrays.toString(favoriteFood) + "]";
+	}
+	
+	public void speakSound(String sound) {
+		System.out.println("사운드 : " + sound);
+	}
+	
+	public void moveAction(String action) {
+		System.out.println("움직임 : " + action);
+		
+	}
+	
+}
+
