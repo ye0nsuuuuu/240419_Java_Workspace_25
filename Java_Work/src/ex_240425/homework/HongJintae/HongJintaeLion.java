ff4c7dccb0c36c294a562ce0cd253179df20ee4b
HongJintae <ghdwlsxo33@gmail.com>
2024-04-25 14:49:19 +0900

    겨우 다했습니다 선생님

---
+++ Java_Work/src/ex_240425/homework/HongJintae/HongJintaeLion.java
@@ -0,0 +1,17 @@
+package ex_240425.homework.HongJintae;
+
+import ex_240425.homework.HongJintae.HongJintaeAnimal;
+
+public class HongJintaeLion extends HongJintaeAnimal {
+	private String place;
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	
+}
+
