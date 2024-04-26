407aea8032ec66c4ba237d1a80e4ef755c4e2a0d
LeeSangYong <lsy3709@gmail.com>
2024-04-26 09:47:21 +0900

    1교시 저장 확인 Book

--- Java_Work/src/ex_240425/AnimalTest.java
+++ Java_Work/src/ex_240425/AnimalTest.java
@@ -24,8 +24,34 @@ public class AnimalTest {
 		// 다형성 부모 타입으로 받기 : SuperA a1 = new A();
 		// 다형성으로 담을 때 좋지만, 다시 원래의 형으로 돌아갈 때는 
 		// 다운 캐스팅을 하여야 한다. 
+		
+		// 큰 타입 = 작은 타입 : 업 캐스팅
 		Animal ani1 = new Cat();
 		Animal ani2 = new Dog();
+		
+		// 형변환을 하기전에 그 타입 맞는지 검사 : instanceof 연산자 이용하기. 
+		// 기본 문법 ,
+		// 작은타입 instanceof 큰타입 => true, false 반환 함. 
+		// 실무에서는 , 처음 부터 담을 때, 제너릭등을 이용해서, 
+		// 모델 타입을 맞춰서 넣습니다. 
+		// 그러면, 꺼내서 사용시, 추가로 유효성 타입 검사를 할 필요가 없음.
+		// 결론, 담을 때 모델별로 (같은 타입)으로 담아라.
+		
+		if(ani1 instanceof Cat) {
+			System.out.println("ani1 은 Cat 타입이 맞습니다.");
+			// 작은 타입 = 큰 타입 : 다운 캐스팅 
+			Cat catTest = (Cat)ani1;
+			catTest.showInfo();
+		} else {
+
+			System.out.println(" Cat 타입이 아닙니다.");
+		}
+		
+		// 작은 타입 = 큰 타입 : 다운 캐스팅 
+//		Cat catTest = (Cat)ani2;
+		// 잘못 변환 하는 실수 
+//		catTest.showInfo();
+		
 		String [] catFoodLiStrings = {"참치캔","사료"};
 		
 		Cat cat2 = new Cat("야옹이", 3, catFoodLiStrings, "상용집");
---
+++ Java_Work/src/ex_240426/BookTest.java
@@ -0,0 +1,16 @@
+package ex_240426;
+
+public class BookTest {
+
+	public static void main(String[] args) {
+		// Book 인스턴스 3개 만들어주세요.
+		// Book 3개를 받는 배열도 만들어주세요.
+		// 배열에 담아서, 반복문으로 출력해주세요.
+		// 제한 시간 8분.
+		// 밴드로 메세지 보내주세요.
+		// 수단과 방법을 가리지 말고 하기.
+		
+
+	}
+
+}
