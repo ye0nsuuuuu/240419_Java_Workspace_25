375091c4ac508d35ee2af0b43a2859e429e20038
LeeSangYong <lsy3709@gmail.com>
2024-04-26 15:19:22 +0900

    6교시 입니다. 앞에 7교시 실수. 죄송해요. 인스턴스와 static 확인 중.

--- Java_Work/src/ex_240426/AccessTest.java
+++ Java_Work/src/ex_240426/AccessTest.java
@@ -8,6 +8,15 @@ class Sample {
 		return b;
 	}
 	
+	//전역 만 확인 해보기.  -> 공용
+	static int tvChannel = 3; // 차달남, 
+	public static void channelUp() {
+		tvChannel++;
+	}
+	public static void channelDown() {
+		tvChannel--;
+	}
+	
 	
 }
 
@@ -25,6 +34,16 @@ public class AccessTest {
 		sample.a = 200;
 		// 직접 읽기
 		System.out.println("sample.a 의 값 : " + sample.a);
+		
+		// 이상용씨, 강루키, 홍사자, 프로젝트 인해서 합숙중, 공용 거실에 쉬는 중. 
+		Sample lsy = new Sample();
+		Sample krk = new Sample();
+		Sample hsj = new Sample();
+		System.out.println("이상용씨가 티비를 " +lsy.tvChannel +"번 채널 시청중");
+		System.out.println("강루키 씨가 UFC 를 보고싶어서 채널 1올렸음." );
+		// 강루키 씨가 채널을 변경. 
+		Sample.channelUp();
+		System.out.println("이상용씨와, 강루키씨, 홍사자는 같이 채널 "+lsy.tvChannel +"번 채널 시청중");
 
 	}
 
