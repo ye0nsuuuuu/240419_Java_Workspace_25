1eb1ef22c5b532029acd12c6821b654979ab9295
LeeSangYong <lsy3709@gmail.com>
2024-04-30 10:47:17 +0900

    240430 2교시 , 상속 후 생성자 호출 순서 확인중.

---
+++ Java_Work/src/ex_240430/SuperConsTest.java
@@ -0,0 +1,33 @@
+package ex_240430;
+
+class A {
+	public A() {
+		System.out.println("생성자 A() 호출");
+	}
+}
+class B extends A{
+	public B() {
+		System.out.println("생성자 B() 호출");
+	}
+}
+class C extends B{
+	public C() {
+		System.out.println("생성자 C() 호출");
+	}
+}
+
+public class SuperConsTest {
+	
+	public static void main(String[] args) {
+	 
+		C c = new C();
+
+	}
+
+}
+
+
+
+
+
+
