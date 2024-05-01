49d1e5b0c4f6a3de610dbabefe1cca560d16a3ef
LeeSangYong <lsy3709@gmail.com>
2024-05-01 16:21:03 +0900

    7교시 미니실습 풀이

--- Java_Work/src/ex_240501_2/Collection_Vector_test.java
+++ Java_Work/src/ex_240501_2/Collection_Vector_test.java
@@ -26,12 +26,14 @@ public class Collection_Vector_test {
 		// 출력 .
 		System.out.println();
 		System.out.println("========================================");
+		System.out.println("벡터의 크기 : " + vectorStrings.size());
 		CollectionUtils.showVectorElem(vectorStrings);
 		// remove , 첫번째 요소 삭제.
 		vectorStrings.remove(0);
 		// 출력.
 		System.out.println();
 		System.out.println("========================================");
+		System.out.println("벡터의 크기 : " + vectorStrings.size());
 		CollectionUtils.showVectorElem(vectorStrings);
 
 		// 원하는 갯수 만큼 더미데이터 넣기 반복문으로.
@@ -40,6 +42,7 @@ public class Collection_Vector_test {
 		// 출력 확인.
 		System.out.println();
 		System.out.println("========================================");
+		System.out.println("벡터의 크기 : " + vectorStrings.size());
 		CollectionUtils.showVectorElem(vectorStrings);
 		System.out.println();
 		System.out.println("Vector의 기본 용량 알아보기 vectorStrings.capacity() : " + vectorStrings.capacity());
@@ -51,10 +54,41 @@ public class Collection_Vector_test {
 		// 출력 확인.
 		System.out.println();
 		System.out.println("========================================");
+		System.out.println("벡터의 크기 : " + vectorStrings.size());
 		CollectionUtils.showVectorElem(vectorStrings);
 		System.out.println();
 		System.out.println("Vector의 기본 용량 알아보기 vectorStrings.capacity() : " + vectorStrings.capacity());
 
+		// 마지막 값 출력.
+		System.out.println("vectorStrings.lastElement();" + vectorStrings.lastElement());
+
+		// 모든 값 삭제.
+		vectorStrings.removeAllElements();
+
+		// 출력 확인.
+		System.out.println();
+		System.out.println("========================================");
+		System.out.println("벡터의 크기 : " + vectorStrings.size());
+		CollectionUtils.showVectorElem(vectorStrings);
+		System.out.println();
+		System.out.println("Vector의 기본 용량 알아보기 vectorStrings.capacity() : " + vectorStrings.capacity());
+		
+		// 오토 박싱(기본형 -> 참조형), 언박싱(참조형 -> 기본형)
+		// 제너릭에 타입은 무조건 참조형만 가능. 
+		Vector<Integer> vectorIntegers = new Vector<Integer>();
+		vectorIntegers.add(Integer.valueOf(30));	
+		vectorIntegers.add(30);
+		//출력. 
+		CollectionUtils.showVectorElem(vectorIntegers);
+		
+		
+		
+
 	}
 
 }
+
+
+
+
+
--- Java_Work/src/util/CollectionUtils.java
+++ Java_Work/src/util/CollectionUtils.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 
 public class CollectionUtils {
 	// Vector의 요소를 출력해주는 메서드 만들기. 
-		public static void showVectorElem(Vector<String> vector) {
+		public static <E> void showVectorElem(Vector<E> vector) {
 			for (int i = 0; i < vector.size(); i++) {
 				if(i == 0) {
 					System.out.print(vector.get(i));	
