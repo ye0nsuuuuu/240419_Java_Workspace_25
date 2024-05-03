c688f6615e0585b94d88c009cca9e2aa7895c846
LeeSangYong <lsy3709@gmail.com>
2024-05-03 17:29:46 +0900

    8교시, 배치 관리자 없이, 크기와, 위치 수동으로 지정해서 배치하기.

--- Java_Work/src/ex_240503/classTest/SwingTest/Swing3.java
+++ Java_Work/src/ex_240503/classTest/SwingTest/Swing3.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,8 +13,8 @@ import javax.swing.JPanel;
 
 
 // 작업순서1, JFrame 그림도구 상속받기. 
-public class Swing2 extends JFrame{
-	public Swing2() {
+public class Swing3 extends JFrame{
+	public Swing3() {
 		// 제목
 		setTitle("300x300 스윙 프레임 만들기");
 		// 창의 엑스 표시를 클릭시 종료하는 코드
@@ -34,19 +36,25 @@ public class Swing2 extends JFrame{
 		// new FlowLayout(정렬방법,수평 간격,수직 간격)
 		
 		// new BorderLayout(수평 간격,수직 간격) : 
-		contentPane.setLayout(new BorderLayout(50,30)); // 컨텐트팬에 FlowLayout
+		//new GridLayout(행,열,수평 간격,수직 간격)
+		contentPane.setLayout(new GridLayout(3,4,20,10)); // 컨텐트팬에 FlowLayout
 		// 배치관리자 달기
 		// new BorderLayout() -> , 위치 , 동, 서,남,북,센터 : 상수 값. 
 		// contentPane.add(요소,위치)
-		contentPane.add(new JButton("OK"),BorderLayout.CENTER); // OK 버튼 달기
-		contentPane.add(new JButton("Cancel"),BorderLayout.EAST); // Cancel 버튼 달기
-		contentPane.add(new JButton("Ignore"),BorderLayout.WEST); // Ignore 버튼 달기
-		contentPane.add(new JButton("Home"),BorderLayout.SOUTH); // Ignore 버튼 달기
+		contentPane.add(new JButton("Home1"));
+		contentPane.add(new JButton("Home2"));
+		contentPane.add(new JButton("Home3"));
+		contentPane.add(new JButton("Home4"));
+		contentPane.add(new JButton("Home5"));
+		contentPane.add(new JButton("Home6"));
+		contentPane.add(new JButton("Home7"));
+		contentPane.add(new JButton("Home8"));
+		contentPane.add(new JButton("Home9"));
+		contentPane.add(new JButton("Home10"));
+		contentPane.add(new JButton("Home11"));
+		contentPane.add(new JButton("Home12"));
 		
-		// 버튼 컴포넌트 생성
-		JButton button = new JButton("Click");
-		// 컨텐트팬 여기에 버튼을 붙이는 코드 
-		contentPane.add(button,BorderLayout.NORTH); // 컨텐트팬에 버튼 부착
+		
 		
 		// 창 보여줄지 여부. 
 		setVisible(true); 
@@ -58,7 +66,7 @@ public class Swing2 extends JFrame{
 
 	public static void main(String[] args) {
 		// 인스턴스를 생성하는 과정 중에, 기본 생성자를 호출해서, 실행함. 
-		Swing2 swing1 = new Swing2() ;
+		Swing3 swing1 = new Swing3() ;
 
 	}
 
