4207d94be460160c58be770ec63ebaaaa626811b
LeeSangYong <lsy3709@gmail.com>
2024-05-03 14:22:12 +0900

    5교시, 자바 게시판 디비내용등 메모

--- Java_Work/src/ex_240503/BoardSample/MemberDAO.java
+++ Java_Work/src/ex_240503/BoardSample/MemberDAO.java
@@ -15,8 +15,8 @@ public class MemberDAO {
     private static final String URL
         = "jdbc:oracle:thin:@localhost:1521:XE";
    
-    private static final String USER = "system"; //DB ID
-    private static final String PASS = "oracle"; //DB 패스워드
+    private static final String USER = "scott"; //DB ID
+    private static final String PASS = "tiger"; //DB 패스워드
     // 회원의 정보들을 출력하는 리스트 
     Member_List mList;
    
