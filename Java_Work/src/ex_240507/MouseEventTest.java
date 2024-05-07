ab1a72c833871d91754add5fa92ec7e80e5844ee
LeeSangYong <lsy3709@gmail.com>
2024-05-07 12:40:15 +0900

    4교시, 마우스 이벤트 처리기 , 익명클래스 방법으로 구현하기. 미니 실습 안내.

--- Java_Work/src/ex_240507/MouseEventTest.java
+++ Java_Work/src/ex_240507/MouseEventTest.java
@@ -1,31 +1,81 @@
 package ex_240507;
 
 import java.awt.Container;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-
-import util.eventListener.MyActionListener;
 import util.eventListener.MyMouseListener;
 
 public class MouseEventTest extends JFrame {
 //pdf19 정답 코드 복붙. 
 	// 코드 확인.
 
-	// 생성자, 
+	// 생성자, -> JLabel 이라는 참조형 변수를 받는 매개변수 선언. 
+	// Hello 라는 문자열 라벨도, 인스턴스 임. 
 	public MouseEventTest(JLabel jaJLabel) {
 
 		setTitle("Mouse 이벤트 예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		// 패널
 		Container c = getContentPane();
 		
-		c.addMouseListener(new MyMouseListener(jaJLabel));
+		// 마우스로 클릭시 이벤트 처리기를 붙이기 작업, 
+		// 방법1) 독립된 클래스 버전
+		// 각 요소마다, 이벤트 처리기가 각각 다르다. 
+//		c.addMouseListener(new MyMouseListener(jaJLabel));
+		
+		// 방법2) 익명 클래스 버전
+		c.addMouseListener(
+				new MouseListener() {
+
+					@Override
+					public void mouseClicked(MouseEvent event) {
+						int x = event.getX(); // 마우스의 클릭 좌표 x
+						int y = event.getY(); // 마우스의 클릭 좌표 y
+						jaJLabel.setLocation(x, y); // (x,y) 위치로 레이블 이동
+						
+					}
+
+					@Override
+					public void mousePressed(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseReleased(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseEntered(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}
+
+					@Override
+					public void mouseExited(MouseEvent e) {
+						// TODO Auto-generated method stub
+						
+					}}
+				
+				);
+		
+		// 배치 관리자가 없음. 자동 정렬이 안됨. 임의의 위치에 지정이 가능.
 		c.setLayout(null);
+		// jaJLabel 의 가로 , 세로 크기
 		jaJLabel.setSize(50, 20); // 레이블의 크기 50x20 설정
+		// jaJLabel의 시작 위치. 
 		jaJLabel.setLocation(30, 30); // 레이블의 위치 (30,30)으로 설정
+		// 라벨을 패널에 붙이기. 
 		c.add(jaJLabel);
+		// 윈도우 창의 크기
 		setSize(200, 200);
+		// 모든 요소를 보열줄지 여부. 
 		setVisible(true);
 	}
 
@@ -38,3 +88,4 @@ public class MouseEventTest extends JFrame {
 	}
 
 }
+
